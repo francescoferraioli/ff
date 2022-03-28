5c3db539d4e1afe589baefba15a27ff61db6b7ca
Francesco Ferraioli <francesco.ferraioli@outlook.com>
2022-03-28 17:10:59 +1000

    Initial implementation of ff

---
+++ index.js
@@ -0,0 +1,46 @@
+#!/usr/bin/env node
+
+const util = require('util');
+const _exec = util.promisify(require('child_process').exec);
+
+const FF_SCRIPT_HOME = process.env.FF_SCRIPT_HOME
+
+if(!FF_SCRIPT_HOME) {
+  console.error("You need to set the FF_SCRIPT_HOME environemnt variable.")
+  return 1;
+}
+
+async function execSafe(command) {
+  try {
+      return await _exec(command);
+  } catch (err) {
+     console.error(err);
+  };
+}
+
+async function exec(command, silent) {
+  const result = await execSafe(command);
+
+  if(!result) return;
+
+  if(result.stdout) {
+    if(!silent) {
+      console.log(result.stdout)
+    }
+    return result.stdout;
+  }
+
+  if(result.stderr) {
+    if(!silent) {
+      console.error(result.stderr)
+    }
+  }
+};
+
+async function run() {
+  const ffObject = process.argv[2]
+  const ffAction = process.argv[3]
+  exec(`sh ${FF_SCRIPT_HOME}/${ffObject}/${ffAction}`)
+}
+
+run()
