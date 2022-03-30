862d5dc3c1a6f9b2523a116e1167741f63f1caf1
Francesco Ferraioli <francesco.ferraioli@outlook.com>
2022-03-30 20:39:20 +1000

    Add help for an object

--- constants.js
+++ constants.js
@@ -4,3 +4,8 @@ if(!FF_PATH) {
   console.error("You need to set the FF_SCRIPT_HOME environment variable.")
   process.exit(1);
 }
+
+export const FF_OBJECT = process.argv[2]
+
+export const FF_ACTION = process.argv[3]
+
--- fs.js
+++ fs.js
@@ -7,6 +7,17 @@ export function getDirectories(source, showHidden = false) {
     .filter(x => showHidden || !isHidden(x))
 }
 
+export function getFiles(source, showHidden = false) {
+	return readdirSync(source, { withFileTypes: true })
+    .filter(dirent => !dirent.isDirectory())
+    .map(dirent => dirent.name)
+    .filter(x => showHidden || !isHidden(x))
+}
+
 export function isHidden(x) {
   return x.startsWith(".")
+}
+
+export function pathJoin(...paths) {
+  return paths.join("/")
 }
