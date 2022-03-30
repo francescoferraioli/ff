08fa47072febd9c201d0a30009c41b99b74ac849
Francesco Ferraioli <francesco.ferraioli@outlook.com>
2022-03-30 17:39:13 +1000

    Extract exec wrapper in it's own file

--- index.js
+++ index.js
@@ -1,41 +1,13 @@
 #!/usr/bin/env node
 
-const util = require('util');
-const _exec = util.promisify(require('child_process').exec);
+import { exec } from './exec.js';
 
 const FF_SCRIPT_HOME = process.env.FF_SCRIPT_HOME
 
 if(!FF_SCRIPT_HOME) {
   console.error("You need to set the FF_SCRIPT_HOME environment variable.")
-  return 1;
+  process.exit(1);
 }
-
-async function execSafe(command) {
-  try {
-      return await _exec(command);
-  } catch (err) {
-     console.error(err);
-  };
-}
-
-async function exec(command, silent) {
-  const result = await execSafe(command);
-
-  if(!result) return;
-
-  if(result.stdout) {
-    if(!silent) {
-      console.log(result.stdout)
-    }
-    return result.stdout;
-  }
-
-  if(result.stderr) {
-    if(!silent) {
-      console.error(result.stderr)
-    }
-  }
-};
 
 async function run() {
   const ffObject = process.argv[2]
