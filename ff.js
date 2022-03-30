48054c5859c757e31ad27aa6f6b74324335fa3ea
Francesco Ferraioli <francesco.ferraioli@outlook.com>
2022-03-30 15:44:34 +1000

    Export run function to ff

---
+++ ff.js
@@ -0,0 +1,16 @@
+import { FF_PATH } from './constants.js';
+import { exec } from './exec.js';
+import { isHelp, listObjects } from './help.js';
+
+export async function ff() {
+  const FF_OBJECT = process.argv[2]
+
+  if(isHelp(FF_OBJECT)) {
+    listObjects()
+    process.exit(0)
+  }
+
+  const FF_ACTION = process.argv[3]
+
+  await exec(`sh ${FF_PATH}/${FF_OBJECT}/${FF_ACTION}`)
+}
--- index.js
+++ index.js
@@ -1,20 +1,5 @@
 #!/usr/bin/env node
 
-import { FF_PATH } from './constants.js';
-import { exec } from './exec.js';
-import { isHelp, listObjects } from './help.js';
+import { ff } from "./ff.js";
 
-async function run() {
-  const FF_OBJECT = process.argv[2]
-
-  if(isHelp(FF_OBJECT)) {
-    listObjects()
-    process.exit(0)
-  }
-
-  const FF_ACTION = process.argv[3]
-
-  await exec(`sh ${FF_PATH}/${FF_OBJECT}/${FF_ACTION}`)
-}
-
-run()
+ff()
