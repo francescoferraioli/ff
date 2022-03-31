ebe6551a6be6d4b85448c8e80c1b4c9c1cf7520d
Francesco Ferraioli <francesco.ferraioli@outlook.com>
2022-03-31 16:25:49 +1000

    Look at common actions too

--- help.js
+++ help.js
@@ -15,5 +15,26 @@ export function listActions() {
 }
 
 export function getActions() {
+  const specificObjectActions = getSpecificObjectActions();
+  const commonObjectActionDependencies = getCommonObjectActionDependencies();
+  return [
+    ...specificObjectActions,
+    ...(
+      commonObjectActionDependencies
+        .filter(x => specificObjectActions.includes(x))
+        .map(x => pathJoin(FF_COMMON_FOLDER, x))
+        .flatMap(getFiles)
+        .filter(x => !specificObjectActions.includes(x))
+    )
+  ]
+}
+
+export function getSpecificObjectActions() {
   return getFiles(pathJoin(FF_PATH, FF_OBJECT));
+}
+
+const FF_COMMON_FOLDER = pathJoin(FF_PATH, ".common");
+
+export function getCommonObjectActionDependencies() {
+  return getDirectories(FF_COMMON_FOLDER);
 }
